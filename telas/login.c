ebc5afef552bbbca2a7deffff87e21d041b6e1ce
Evandro Myller <evandro.vieira1@aluno.unip.br>
2020-09-20 22:21:31 -0300

    Adiciona tela de login

---
+++ telas/login.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	char usuario[200];
+	char senha[10];
+
+	// Lê usuário da tela
+	printf("Insira seu usuário: ");
+	fgets(usuario, 200, stdin);
+
+	// Lê a senha
+	printf("Insira a sua senha: ");
+	fgets(senha, 10, stdin);
+
+	// Autentica usuário
+	printf("Bem vindo, %s!\n", usuario);
+
+	return 0;
+}
