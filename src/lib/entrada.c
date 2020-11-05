80020467ec165eb4bb5516b190ee2815390ea8b0
Evandro Myller <evandro.vieira1@aluno.unip.br>
2020-11-05 01:04:38 -0300

    Adiciona função para leitura de input

---
+++ src/lib/entrada.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+char* leia_string(char* pergunta, char* resposta) {
+	/*
+	Lê uma string a partir de uma pergunta feita
+	*/
+	printf(pergunta);  // Exibe pergunta
+	fgets(resposta, 100, stdin);  // Lê resposta
+	resposta[sizeof(resposta) - 1] = '\0';  // Remove quebra de linha ao final
+}
---
+++ src/lib/entrada.h
@@ -0,0 +1,8 @@
+#ifndef ENTRADA_H
+#define ENTRADA_H
+
+char* leia_string(char* pergunta, char* resposta);
+
+// int leia_digito(char pergunta);
+
+#endif
