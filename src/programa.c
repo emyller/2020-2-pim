b53446df98f23194806d729e103a9de4f2695ffe
Evandro Myller <evandro.vieira1@aluno.unip.br>
2020-11-05 01:29:05 -0300

    Introduz um menu no programa

--- src/programa.c
+++ src/programa.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "lib/entrada.h"
 
 
 int main() {
+	int opcao;
 
+	leia_digito("Insira opção: ", &opcao);
+
+	switch (opcao) {
+		case 1:
+			printf("Yay");
+			break;
+		default:
+			printf("Opção inválida. Tente novamente.");
+	}
 }
