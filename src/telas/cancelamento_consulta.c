b8f3be39795f7af685dd52c2c528fea271c8945c
Carolina Model <carolina.silva126@aluno.unip.br>
2020-11-18 19:44:28 -0300

    Adiciona tela de cancelamento de consulta

---
+++ src/telas/cancelamento_consulta.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/entrada.h"
+
+int cancelamento_consulta() {
+
+    char nome_paciente[100]; //nome do paciente
+    char data[10]; //data da consulta
+    char hora[6]; //horário da consulta
+    int opcao;
+
+
+    puts("Cancelamento de Consulta\n\n");
+
+        //para fazer o cancelamento de uma consulta o usuario precisa inserir o nome do paciente para encontrar seu cadastro
+    leia_string("Nome do paciente ", nome_paciente); 
+    
+    leia_digito("O paciente gostaria de remarcar a consulta?\n(1)Sim\n(2)Não\n\n", &opcao);//o usuário pergunta ao paciente se ele deseja remarcar a consulta
+    
+        //caso o paciente queira remarcar, é feito novamente um processo de agendamento
+    if (opcao == 1) { 
+        leia_string("Qual a data da consulta? ", data);
+
+        leia_string("Horário da consulta: ", hora);
+
+        puts("Consulta agendada com sucesso!");
+    }
+    
+    else if (opcao == 2) { //caso o paciente não queira remarcar, sua consulta é apenas cancelada
+        puts("Consulta cancelada");
+    }
+    else {
+        return 1;
+    }
+return 0;
+
+}
---
+++ src/telas/cancelamento_consulta.h
@@ -0,0 +1,6 @@
+#ifndef CANCELAMENTO_CONSULTA_H
+#define CANCELAMENTO_CONSULTA_H
+
+int cancelamento_consulta();
+
+#endif
