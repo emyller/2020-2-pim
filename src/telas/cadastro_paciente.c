07c467767c6e94a60535a1beecc5df6b2ad78598
MauricioSchaefer <contato.schaefer.m@gmail.com>
2020-11-20 21:59:38 -0300

    Inclui a tela de cadastro de pacientes

---
+++ src/telas/cadastro_paciente.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib/entrada.h"
+
+
+int cadastro_paciente()  { 
+
+	char nome[200]; // Nome do cliente.
+	char rg[10]; // RG do cliente.
+	char cpf[12]; // CPF do cliente.
+	char numero_contato[12]; // Número de contato do cliente.
+	char cep[11]; // CEP do cliente.
+	char endereco[200]; // Endereço do cliente com numero da casa.
+	char data_nascimento[20]; // Data de Nascimento do cliente.
+	char plano_saude[50]; // Plano de saude do cliente.
+	int resposta; // RESPOSTA para o comando "do while" em confirmações se os dados estão corretos ou não.
+	int casos;
+
+	puts("--  Cadastro de pacientes. --");
+
+	leia_string("Insira o nome do paciente: ", nome);
+
+	do {
+		leia_string("Digite o RG do paciente: ", rg);
+		leia_digito("CPF digitado corretamente? [0/1] ", &resposta);
+	}
+	while (resposta == 0);
+
+	do {
+		leia_string("Digite o CPF do paciente: ", cpf);
+		leia_digito("RG digitado corretamente? [0/1] ", &resposta);
+	}
+	while (resposta == 0);
+
+	do {
+		leia_string("Insira um número de contato: ", numero_contato);
+		leia_string("Insira a data de nascimento: ", data_nascimento);
+		leia_string("Insira o CEP: ", cep);
+		leia_string("Insira o endereço: ", endereco);
+		leia_digito("Cliente possui plano de saude? [0/1] ", &casos);
+			if (casos == 1) {
+				leia_string("Digite o plano de saude do paciente", plano_saude);
+			}
+			else {
+				puts("Cliente em consulta particular");
+			}
+			
+		leia_digito("Todos os dados foram preenchidos corretamente: [0/1]", &resposta);
+	}
+	while (resposta == 0);
+}	
---
+++ src/telas/cadastro_paciente.h
@@ -0,0 +1,6 @@
+#ifndef CADASTRO_PACIENTE_H
+#define CADASTRO_PACIENTE_H
+
+int cadastro_paciente();
+
+#endif
