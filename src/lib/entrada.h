6115350acf96909c788d73e6859b23c1f1b4822f
Evandro Myller <evandro.vieira1@aluno.unip.br>
2020-11-23 01:02:06 -0300

    Atualiza headers da biblioteca de entrada

--- src/lib/entrada.h
+++ src/lib/entrada.h
@@ -1,7 +1,11 @@
 #ifndef ENTRADA_H
 #define ENTRADA_H
 
-void leia_string(char* pergunta, char* resposta);
+void leia_resposta(char* pergunta, char* resposta);
+
+void leia_resposta_formato(char* pergunta, char* formato, char* resposta);
+
+int leia_decisao(char* pergunta);
 
 void leia_digito(char* pergunta, int* resposta);
 
