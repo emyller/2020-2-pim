47c6b86ece99f7788af2f689855c4a87cf95425e
Evandro Myller <evandro.vieira1@aluno.unip.br>
2020-11-23 01:28:45 -0300

    Permite cadastro de paciente durante agendamento

--- src/telas/agendamento_consulta.c
+++ src/telas/agendamento_consulta.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "../lib/arquivos.h"
 #include "../lib/entrada.h"
+#include "./cadastro_paciente.h"
 
 #define AGENDAMENTO_CONSULTA_ARQUIVO "agendamento_consulta.txt"
 
@@ -16,30 +17,25 @@ int agendamento_consulta() {
 
 	puts("Agendamento de Consulta");
 
+	// Executa tela de cadastro do paciente, se necessário
 	leia_decisao("Possui cadastro", &ja_possui_cadastro);
-
-	// Caso o paciente já tenha cadastro
-	if (ja_possui_cadastro) {
-		leia_resposta("Nome do paciente", nome_paciente);
-
-		// TODO: Exibir disponibilidade de médicos e horários
-		leia_resposta("Especialidade médica", especialidade_medica);
-		leia_resposta("Nome do(a) médico(a)", nome_medico);
-		leia_resposta_formato("Data da consulta", "YYYY-MM-DD", data);
-		leia_resposta_formato("Hora da consulta", "HH:MM", hora);
-
-		// Insere resultados no arquivo CSV
-		escreve_linha_csv(
-			AGENDAMENTO_CONSULTA_ARQUIVO, 5,
-			nome_paciente, especialidade_medica, nome_medico, data, hora
-		);
-
-		puts("Consulta agendada com sucesso!");
+	if (!ja_possui_cadastro) {
+		cadastro_paciente();
 	}
 
-	// Caso ainda não tenha cadastro
-	else {
-		// TODO: Executa função de cadastro e volta
-		puts("Faça o cadastro do paciente.");
-	}
+	leia_resposta("Nome do paciente", nome_paciente);
+
+	// TODO: Exibir disponibilidade de médicos e horários
+	leia_resposta("Especialidade médica", especialidade_medica);
+	leia_resposta("Nome do(a) médico(a)", nome_medico);
+	leia_resposta_formato("Data da consulta", "YYYY-MM-DD", data);
+	leia_resposta_formato("Hora da consulta", "HH:MM", hora);
+
+	// Insere resultados no arquivo CSV
+	escreve_linha_csv(
+		AGENDAMENTO_CONSULTA_ARQUIVO, 5,
+		nome_paciente, especialidade_medica, nome_medico, data, hora
+	);
+
+	puts("Consulta agendada com sucesso!");
 }
