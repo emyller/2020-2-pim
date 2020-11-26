f5883378c6234441cba3d3596b2e78d6a902e476
Evandro Myller <evandro.vieira1@aluno.unip.br>
2020-11-26 16:03:35 -0300

    Implementa verificação de dados no login

--- src/telas/login.c
+++ src/telas/login.c
@@ -1,4 +1,7 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+#include "../lib/arquivos.h"
 #include "../lib/entrada.h"
 
 
@@ -10,10 +13,23 @@ int login() {
 	leia_resposta("Usuário", usuario);
 	leia_resposta("Senha", senha);
 
-	// TODO: Verificar dados
+	// Lê arquivo de usuários
+	char linhas[100][200];  // Array de linhas
+	leia_arquivo("login.txt", 200, linhas);
 
-	// Exibe mensagem de boas vindas
-	printf("Bem vindo, %s!\n", usuario);
+	// Tenta encontrar usuário e senha
+	char linha[2][100];  // Array de valores em cada linha
+	for (int i = 0; i < 100; i++) {
+		// Compara usuário e senha com valores das linhas
+		leia_linha_csv(linhas[i], 100, linha);
+		if (strcmp(usuario, linha[0]) == 0 && strcmp(senha, linha[1]) == 0) {
+			// Exibe mensagem de boas vindas
+			printf("Bem vindo(a), %s!\n", usuario);
+			return true;
+		}
+	}
 
-	return 0;
+	// Não achou usuário e senha
+	puts("Usuário ou senha incorretos.");
+	return false;
 }
