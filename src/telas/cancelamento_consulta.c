caa5f63d5b78476a130a35afcd7cd78792e589fa
Carolina Model <carolina.silva126@aluno.unip.br>
2020-11-27 11:55:26 -0300

    Adiciona funçao agendamento de consulta

--- src/telas/cancelamento_consulta.c
+++ src/telas/cancelamento_consulta.c
@@ -3,6 +3,7 @@
 #include "../lib/arquivos.h"
 #include "../lib/entrada.h"
 #include "../lib/utils.h"
+#include "./agendamento_consulta.h"
 
 #define CANCELAMENTO_CONSULTA_ARQUIVO "cancelamento_consulta.txt"
 
@@ -18,22 +19,18 @@ int cancelamento_consulta() {
 	leia_resposta("Nome do paciente", nome_paciente);
 	leia_decisao("Remarcar consulta", &remarcar_consulta);
 
-	// Insere resultados no arquivo CSV
-	escreve_linha_csv(
-		CANCELAMENTO_CONSULTA_ARQUIVO, 3,
-		nome_paciente, data, hora  // TODO: Usar data e hora da consulta cancelada
-	);
-
-	// Caso o paciente queira remarcar, é feito um processo de reagendamento
+	// Caso o paciente queira remarcar, executa tela de cadastro do paciente
 	if (remarcar_consulta) {
-		// TODO: Chama função de agendamento
-		leia_resposta_formato("Data da consulta", "dd-mm-yyyy", data);
-		leia_resposta_formato("Hora da consulta", "hh:mm", hora);
-		exibe_sucesso("Consulta reagendada.");
+		agendamento_consulta();
 	}
 
 	// Caso o paciente não queira remarcar, sua consulta é apenas cancelada
 	else {
+		// Insere resultados no arquivo CSV
+		escreve_linha_csv(
+			CANCELAMENTO_CONSULTA_ARQUIVO, 3,
+			nome_paciente, data, hora  // TODO: Usar data e hora da consulta cancelada
+		);
 		exibe_sucesso("Consulta cancelada.");
 	}
 
